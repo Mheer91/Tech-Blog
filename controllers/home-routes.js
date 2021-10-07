36520693345cf069e99084f740db4be2057e7251
Michael Heer <mheer91@icloud.com>
2021-10-07 17:01:31 -0500

    Handlebars progress

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req,res) => {
     try {
@@ -11,7 +12,7 @@ router.get('/', async (req,res) => {
 
         console.log(post);
 
-        res.render('home', { post, loggedIn: req.session.loggedIn })
+        res.render('home', { post, loggedIn: req.session.loggedIn, userId: req.session.user_id  })
     }
     catch (err) {
         res.status(500).json(err);
@@ -22,7 +23,7 @@ router.get('/post/:id', async (req,res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: { model: User }
-        })
+        });
 
         const commentData = await Comment.findAll({
             where: {
@@ -42,6 +43,47 @@ router.get('/post/:id', async (req,res) => {
     catch (err) {
         res.status(500).json(err);
     }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            include: [{ model: Post}, { model: Comment}]
+        });
+
+        const user = userData.get({ plain: true });
+
+        console.log(user)
+
+        res.render('dashboard', { user, loggedIn: req.session.loggedIn, userId: req.session.user_id })
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard/:id', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [{ model: Post }, { model: Comment }]
+        });
+
+        const user = userData.get({ plain: true });
+
+        console.log(user)
+
+        res.render('dashboard', { user, loggedIn: req.session.loggedIn, userId: req.session.user_id  })
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', async (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+    }
+    res.render('login', { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+const login = async (event) => {
+    event.preventDefault();
+  
+  
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    console.log(username, password)
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      console.log(response)
+  
+      if (response.ok) {
+        document.location.replace('/dashboard')
+      }
+      else {
+        alert('filleralert')
+      }
+    }
+  };
+
+  const createAccount = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    console.log(username, passowrd)
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+        console.log(response)
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+
+  document.querySelector('#login').addEventListener('click', login);
+  document.querySelector('#createAccount').addEventListener('click', createAccount);
